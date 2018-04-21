987638f857398be3176b6f61635eaf49adc5f326
Courtney Severson <courtneyseverson8@gmail.com>
2018-04-21 10:58:03 -0500

    added files

---
+++ TrashCollector/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TrashCollector
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            //need to add in filters here, for each employee/user
+        }
+    }
+}
---
+++ TrashCollector/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrashCollector.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            //dynamic object that passes information between controller and view, only available if you return a view at the end of the method
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Us";
+
+            return View();
+        }
+
+        //public ActionResult Schedule(string user)
+        //{
+        //    //var schedule = ;
+        //    //return ContentResult; this could return user content that shows exact numbers
+            //would you return a FIle Result? 
+        //}
+
+        //public ActionResult GoogleMap()
+        //{
+        //    //pass through google API, show a partial view
+        //}
+        ////partial result can view a part of a webpage
+
+
+        //[HttpGet]
+
+        //Need to create controllers
+        //enter my info and sign up to receive a pickup
+
+        //Registered user - change pickup to fit schedule
+        //see how much you owe this month
+        //specify a period of time you don't want a pickup
+
+        //Trash worker - see customers on my route using google maps integration
+        //see which customers in zip code are receiving a pickup today
+
+
+        //Authorize Attribute - controller Level Filter
+        [Authorize (Roles=Employee)]
+        //create a table that shows employees
+        //can show [HandleError which would allow you to give exceptions]
+
+    }
+}
